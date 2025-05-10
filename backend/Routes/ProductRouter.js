4fd68d5cef5abd32fdfc25a00772e621858f5b41
Mohit Rana <mohitr8998@gmail.com>
2025-05-10 22:06:45 +0530

    created api for auth

---
+++ backend/Routes/ProductRouter.js
@@ -0,0 +1,18 @@
+const ensureAuthenticated = require('../Middlewares/Auth')
+
+const router = require('express').Router()
+
+router.get('/',ensureAuthenticated, (req, res)=>{
+    res.status(200).json([
+        {
+            name: "mobile",
+            price: 12000
+        },
+        {
+            name: "tv",
+            price: 15000
+        }
+    ])
+})
+
+module.exports = router
